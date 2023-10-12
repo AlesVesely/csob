c9d2a2b73060569943b4a7fa7a238cdd041b8c04
AlesVesely <vesely-ales@seznam.cz>
2023-10-12 12:38:30 +0200

    x1

---
+++ src/CsobNG/Admin/Data/Endpoints.cs
@@ -0,0 +1,14 @@
+﻿namespace Admin.Data
+{
+	public class Endpoints
+	{
+		public static Dictionary<string, string> HealthEndpoints =
+			new Dictionary<string, string>()
+			{
+				{"ClientAPI", "http://localhost:5208" },
+				{"LoanRequestAPI", "http://localhost:5174" },
+				{"EmailsAPI", "http://localhost:5032" },
+				{"ClientBFF" , "http://localhost:5025"},
+			};
+	}
+}
